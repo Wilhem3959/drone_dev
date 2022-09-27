c0caa53f9026d3f1e6a7ac5da609403ece6d7bee
Wilhem3959 <36482878+Wilhem3959@users.noreply.github.com>
2022-09-27 15:10:06 -0400

    1.11 Version

--- lib/drone_settings_page.dart
+++ lib/drone_settings_page.dart
@@ -17,6 +17,10 @@ class DroneSettingsPage extends StatefulWidget {
 
 class _SettingsPageState extends State<DroneSettingsPage> {
   TimeOfDay selectedTime = TimeOfDay.now();
+  DateTimeRange selectedDateRange = DateTimeRange(
+    start: DateTime.now(),
+    end: DateTime.now().add(const Duration(days: 7)),
+  );
 
   TextStyle headingStyle = const TextStyle(
       fontSize: 16, fontWeight: FontWeight.w600, color: Color(0xFF0437F2));
@@ -32,96 +36,104 @@ class _SettingsPageState extends State<DroneSettingsPage> {
   );
   TextStyle descStyleIOS = const TextStyle(color: CupertinoColors.inactiveGray);
 
-  get child => null;
-
   @override
   Widget build(BuildContext context) {
-    String title;
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch().copyWith(
-            primary: const Color(0xFF0437F2),
-            secondary: const Color(0xFFD4AF37),
-            background: Colors.white),
-      ),
-      home: Scaffold(
-        appBar: AppBar(title: const Text("Drone: Name of drone settings")),
-        drawer: const HeaderDrawer(),
-        body: SingleChildScrollView(
-          child: Container(
-            padding: const EdgeInsets.all(12),
-            alignment: Alignment.center,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Text("Sensors", style: headingStyle),
-                  ],
-                ),
-                ListTile(
-                  leading: const Icon(Icons.phonelink_lock_outlined),
-                  title: const Text("EID Detection sensor"),
-                  trailing: Switch(
-                      value: lockAppSwitchVal,
-                      activeColor: const Color(0xFFD4AF37),
-                      onChanged: (val) {
-                        setState(() {
-                          lockAppSwitchVal = val;
-                        });
-                      }),
-                ),
-                const Divider(),
-                ListTile(
-                  leading: const Icon(Icons.fingerprint),
-                  title: const Text("Radio Jammer"),
-                  trailing: Switch(
-                      value: fingerprintSwitchVal,
-                      activeColor: const Color(0xFFD4AF37),
-                      onChanged: (val) {
-                        setState(() {
-                          fingerprintSwitchVal = val;
-                        });
-                      }),
-                ),
-                const Divider(),
-                ListTile(
-                  leading: const Icon(Icons.lock),
-                  title: const Text("Electronic magnetic pulse"),
-                  trailing: Switch(
-                      value: changePassSwitchVal,
-                      activeColor: const Color(0xFFD4AF37),
-                      onChanged: (val) {
-                        setState(() {
-                          changePassSwitchVal = val;
-                        });
-                      }),
-                ),
-                const Divider(),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Text("Schedule", style: headingStyle),
-                  ],
-                ),
-                ListTile(
-                  leading: const Icon(Icons.access_time),
-                  title: Text("${selectedTime.hour}:${selectedTime.minute}"),
-                  trailing: IconButton(
-                    icon: const Icon(Icons.edit),
-                    onPressed: () {
-                      _selectTime(context);
-                    },
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch().copyWith(
+              primary: const Color(0xFF0437F2),
+              secondary: const Color(0xFFD4AF37),
+              background: Colors.white),
+        ),
+        home: Scaffold(
+          appBar: AppBar(title: const Text("Drone: Name of drone settings")),
+          drawer: const HeaderDrawer(),
+          body: SingleChildScrollView(
+            child: Container(
+              padding: const EdgeInsets.all(12),
+              alignment: Alignment.center,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text("Sensors", style: headingStyle),
+                    ],
                   ),
-                ),
-              ],
+                  ListTile(
+                    leading: const Icon(Icons.phonelink_lock_outlined),
+                    title: const Text("EID Detection sensor"),
+                    trailing: Switch(
+                        value: lockAppSwitchVal,
+                        activeColor: const Color(0xFFD4AF37),
+                        onChanged: (val) {
+                          setState(() {
+                            lockAppSwitchVal = val;
+                          });
+                        }),
+                  ),
+                  const Divider(),
+                  ListTile(
+                    leading: const Icon(Icons.fingerprint),
+                    title: const Text("Radio Jammer"),
+                    trailing: Switch(
+                        value: fingerprintSwitchVal,
+                        activeColor: const Color(0xFFD4AF37),
+                        onChanged: (val) {
+                          setState(() {
+                            fingerprintSwitchVal = val;
+                          });
+                        }),
+                  ),
+                  const Divider(),
+                  ListTile(
+                    leading: const Icon(Icons.lock),
+                    title: const Text("Electronic magnetic pulse"),
+                    trailing: Switch(
+                        value: changePassSwitchVal,
+                        activeColor: const Color(0xFFD4AF37),
+                        onChanged: (val) {
+                          setState(() {
+                            changePassSwitchVal = val;
+                          });
+                        }),
+                  ),
+                  const Divider(),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text("Schedule", style: headingStyle),
+                    ],
+                  ),
+                  ListTile(
+                    leading: const Icon(Icons.access_time),
+                    title: Text(
+                        "Time: ${selectedTime.hour}:${selectedTime.minute}"),
+                    trailing: IconButton(
+                      icon: const Icon(Icons.edit),
+                      onPressed: () {
+                        _selectTime(context);
+                      },
+                    ),
+                  ),
+                  ListTile(
+                    leading: const Icon(Icons.calendar_today),
+                    title: Text(
+                        "Date: ${selectedDateRange.start.toString().split(' ')[0]} to ${selectedDateRange.end.toString().split(' ')[0]}"),
+                    trailing: IconButton(
+                      icon: const Icon(Icons.edit),
+                      onPressed: () {
+                        _show();
+                      },
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
-        ),
-      ),
-    );
+        ));
   }
 
   _selectTime(BuildContext context) async {
@@ -136,6 +148,23 @@ class _SettingsPageState extends State<DroneSettingsPage> {
     if (timeOfDay != null && timeOfDay != selectedTime) {
       setState(() {
         selectedTime = timeOfDay;
+      });
+    }
+  }
+
+  void _show() async {
+    final DateTimeRange? result = await showDateRangePicker(
+      context: context,
+      firstDate: DateTime(2022, 1, 1),
+      lastDate: DateTime(2030, 12, 31),
+      currentDate: DateTime.now(),
+      saveText: 'Done',
+    );
+
+    if (result != null) {
+      setState(() {
+        selectedDateRange = result;
+        print(selectedDateRange);
       });
     }
   }
